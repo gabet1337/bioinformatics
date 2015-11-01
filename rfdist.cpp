95f3b5db696632da7976638a1afee5df2ccf97a9
Peter Gabrielsen <peterg@cs.au.dk>
2015-11-01 07:06:03 -0800

    rooted the trees at the same leaf

--- rfdist.cpp
+++ rfdist.cpp
@@ -1,6 +1,8 @@
 #include "newick_parser.hpp"
 #include <iostream>
 #include <vector>
+#include <map>
+#include <queue>
 using namespace std;
 typedef pair<int,double> PID;
 typedef vector<PID> vid;
@@ -19,6 +21,26 @@ int main(int argc, char *argv[]) {
   node* tree2 = p.parse(argv[2]);
 
   //root the two trees at the same leaf
+  //1. find a leaf in tree1 and make that root.
+  queue<node*> q;
+  q.push(tree1);
+  while (!q.empty()) {
+    node* f = q.front(); q.pop();
+    if (f->name != "") {tree1 = f; break;}
+    for (auto &n : f->adj_list)
+      if (n.first != f) q.push(n.first);
+  }
+  cout << "tree1 rooted at: " << tree1->name << endl;
+  //2. find same leaf in tree2 and make that root
+  q = queue<node*>();
+  q.push(tree2);
+  while (!q.empty()) {
+    node* f = q.front(); q.pop();
+    if (f->name == tree1->name) {tree2 = f; break;}
+    for (auto &n : f->adj_list)
+      if (n.first != f) q.push(n.first);
+  }
+  cout << "tree2 rooted at: " << tree2->name << endl;
 
   //make a depth first numbering of the leaves in T1
 
